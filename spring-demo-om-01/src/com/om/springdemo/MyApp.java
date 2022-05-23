625dab23d9e330cae25a227181f37931ff6a7852
Omar Qasirov <omarbmohamad@gmail.com>
2022-05-22 23:43:30 -0500

    Adding classes

---
+++ spring-demo-om-01/src/com/om/springdemo/MyApp.java
@@ -0,0 +1,14 @@
+package com.om.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+
+		//create object
+		BaseballCoach baseballCoach = new BaseballCoach();
+		
+		//use object
+		System.out.println(baseballCoach.getDailyWorkout());
+	}
+
+}
